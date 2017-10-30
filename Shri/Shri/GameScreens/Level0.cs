ad680e505216990183b415a20e442a0254b23b4b
PlGGS <picklepig7@comcast.net>
2017-10-30 10:38:02 -0500

    Properly draw left and right walls for future collision checking

--- Shri/Shri/GameScreens/Level0.cs
+++ Shri/Shri/GameScreens/Level0.cs
@@ -14,10 +14,12 @@ namespace Shri
         Texture2D txrPlayer;
         Sprite sprPlayer;
         Texture2D txrBackground;
+        Texture2D txrWallTall;
+        Sprite sprWallLeft;
+        Sprite sprWallRight;
         Texture2D txrMediumFont;
         FramedSprite sprMediumFont;
         Font fntMediumFont;
-        float scale;
 
         public Level0()
         {
@@ -34,14 +36,26 @@ namespace Shri
             base.LoadContent(contentManager);
             
             txrPlayer = contentManager.GetTexture("Content\\Sprites\\bud.png");
-            sprPlayer = new Sprite(txrPlayer, 
+            sprPlayer = new Sprite(txrPlayer,
                 new Vector2((Shri.Instance.Window.ClientBounds.Width / 2),
                 (Shri.Instance.Window.ClientBounds.Height / 2 + 200)),
-                Color.White, new Vector2((txrPlayer.Width) / 2, (txrPlayer.Height) / 2), 
-                true, 250, 1.0f, 90);
-            sprPlayer.Scale = new Vector2(0.2f, 0.2f); //Always make sure to set custom scale after instance creation
+                Color.White, new Vector2((txrPlayer.Width) / 2, (txrPlayer.Height) / 2),
+                true, 250, 1.0f, 90)
+            {
+                Scale = new Vector2(0.2f, 0.2f) //Always make sure to set custom scale after instance creation
+            };
 
             txrBackground = contentManager.GetTexture("Content\\Sprites\\baseLevel.png");
+
+            txrWallTall = contentManager.GetTexture("Content\\Sprites\\black.png");
+            sprWallLeft = new Sprite(txrWallTall, Vector2.Zero, Color.Black, Vector2.Zero)
+            {
+                Scale = new Vector2(1f, 48f) //Always make sure to set custom scale after instance creation
+            };
+            sprWallRight = new Sprite(txrWallTall, new Vector2(Shri.Instance.Window.ClientBounds.Width - txrWallTall.Width, 0), Color.Black, Vector2.Zero)
+            {
+                Scale = new Vector2(1f, 48f) //Always make sure to set custom scale after instance creation
+            };
 
             txrMediumFont = contentManager.GetTexture("Content\\Fonts\\medium-font.png");
             sprMediumFont = new FramedSprite(18, 4, 0, txrMediumFont, Vector2.Zero, Color.White, false);
@@ -64,9 +78,11 @@ namespace Shri
             base.Draw(spriteBatch);
 
             spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend, SamplerState.PointWrap);
-            fntMediumFont.DrawString(spriteBatch, "Hello, World", new Vector2(0, 0)); //TODO fix this
-            spriteBatch.Draw(txrBackground, Vector2.Zero, Color.White);
+            fntMediumFont.DrawString(spriteBatch, "Hello, World", new Vector2(100, 100)); //TODO fix this
+            //spriteBatch.Draw(txrBackground, Vector2.Zero, Color.White);
             sprPlayer.Draw(spriteBatch);
+            sprWallLeft.Draw(spriteBatch);
+            sprWallRight.Draw(spriteBatch);
             spriteBatch.End();
         }
     }
